b9b8894ae05e50bab190aaa7e437bbbeb2ba884f
大沢 和宏 <yappo>
2009-02-10 12:57:59 +0900

    first commit

---
+++ lib/Any/Mouse.pm
@@ -0,0 +1,43 @@
+package Any::Mouse;
+
+use strict;
+use warnings;
+use base 'Any::Moose';
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Any::Mouse -
+
+=head1 SYNOPSIS
+
+  use Any::Mouse;
+
+=head1 DESCRIPTION
+
+Any::Mouse is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo <at> shibuya <döt> plE<gt>
+
+=head1 SEE ALSO
+
+=head1 REPOSITORY
+
+  svn co http://svn.coderepos.org/share/lang/perl/Any-Mouse/trunk Any-Mouse
+
+Any::Mouse is Subversion repository is hosted at L<http://coderepos.org/share/>.
+patches and collaborators are welcome.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
